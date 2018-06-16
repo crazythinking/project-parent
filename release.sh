d69db4142a865088a9c1f1f67df165ca46326063
crazy_thinking <crazy_thinking@163.com>
2018-06-16 18:51:30 +0800

    init

---
+++ release.sh
@@ -0,0 +1,12 @@
+sed -i "s/3.1-SNAPSHOT/3.1.RELEASE/g" pom.xml */pom.xml */*/pom.xml
+# sed -i "s/trunk/tags\/3.1.RELEASE/g" pom.xml
+svn ci -m "prepare release 3.1.RELEASE"
+# mvn clean deploy
+svn copy http://101.132.40.129/svn/repositories/smartstar/trunk/project http://101.132.40.129/svn/repositories/smartstar/tags/project-3.1.RELEASE -m "3.1.RELEASE"
+
+# sed -i "s/tags\/1.0.1.RELEASE/trunk/g" pom.xml
+sed -i "s/3.1.RELEASE/3.2-SNAPSHOT/g" pom.xml */pom.xml */*/pom.xml
+svn ci -m "prepare new 3.2-SNAPSHOT"
+svn copy http://101.132.40.129/svn/repositories/smartstar/trunk/project http://101.132.40.129/svn/repositories/smartstar/branches/project-3.2-SNAPSHOT -m "begining of 3.2-SNAPSHOT"
+# mvn clean deploy
+
